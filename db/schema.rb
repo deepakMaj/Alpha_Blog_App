69a68ac43bc225a5f19c92669895b1b4e0121ebb
DeepakMaj <deepakmahajan269@gmail.com>
2019-10-03 21:12:44 +0530

    backend complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_03_110352) do
+ActiveRecord::Schema.define(version: 2019_10_03_153250) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -20,11 +20,12 @@ ActiveRecord::Schema.define(version: 2019_10_03_110352) do
     t.integer "user_id"
   end
 
-  create_table "user", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
