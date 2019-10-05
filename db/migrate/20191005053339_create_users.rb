11f0a65cfe1430bfdbaefcb7e1aa1d2d6d6b517c
DeepakMaj <deepakmahajan269@gmail.com>
2019-10-05 11:19:55 +0530

    Updated

--- db/migrate/20191005053339_create_users.rb
+++ db/migrate/20191005053339_create_users.rb
@@ -3,7 +3,8 @@ class CreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       t.string :username
       t.string :email
-      t.timestamps
+      t.datetime :created_at
+      t.datetime :updated_at
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_03_153250) do
+ActiveRecord::Schema.define(version: 2019_10_05_054208) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -23,8 +23,8 @@ ActiveRecord::Schema.define(version: 2019_10_03_153250) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string "password_digest"
   end
 
