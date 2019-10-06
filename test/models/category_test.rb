08846b9caa538643b528447a7644409dea8fe922
DeepakMaj <deepakmahajan269@gmail.com>
2019-10-06 11:34:55 +0530

    add categories model

---
+++ test/models/category_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class CategoryTest < ActiveSupport::TestCase
+
+  def setup
+    @category = Category.new(name: "sports")
+  end
+
+  test "category should be valid" do
+    assert @category.valid? #assert ensure that test is true
+  end
+
+  test "name should be present" do
+    @category.name = " "
+    assert_not @category.valid? #assert_not ensure that test is false
+  end
+
+  test "name should be unique" do
+    @category.save
+    category2 = Category.new(name: "sports")
+    assert_not category2.valid?
+  end
+
+  test "name not too long" do
+    @category.name = "a" * 26
+    assert_not @category.valid?
+  end
+
+  test "name not too short" do
+    @category.name = "a" * 2
+    assert_not @category.valid?
+  end
+
+end
